45b62cfbf003a072fe3660a6257c381155ec67ee
andi lovetto <andilovetto@gmail.com>
2023-07-10 08:46:03 -0600

    complete AR association video

--- Gemfile
+++ Gemfile
@@ -52,8 +52,9 @@ group :development, :test do
   gem 'pry'
   gem 'rspec-rails'
   gem 'capybara'
-  gem 'launchy' # save and open page
+  gem 'launchy' # save_and_open_page
   gem 'simplecov'
+  gem 'shoulda-matchers'
 
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+    @prince = Artist.create!(name: 'Prince')
+    @purple = @prince.songs.create!(title: "Purple Rain", length: 345, play_count: 890)
+    @beret = @prince.songs.create!(title: "Raspberry Beret", length: 254, play_count: 8990)
